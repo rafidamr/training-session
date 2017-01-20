936368b1f24c8b84500c44caea4e3a6783516973
rapidesign <fidrafid@gmail.com>
2017-01-20 18:55:07 +0700

    Add files via upload

--- validation.php
+++ validation.php
@@ -9,6 +9,7 @@
         $date = strftime("%B %d, %Y");//date
 
 		$con = mysqli_connect("localhost", "root", "", "test"); //Connect to server and database
+//		$con = mysqli_connect("localhost", "debian-sys-maint", "whQavSxR95YLYv80", "oprec_arc_itb_ac_id"); //Connect to server and database
         if (!$con) die("Connection failed: " . mysqli_connect_error()); //check connection
         
         mysqli_query($con,"INSERT INTO training_session ( nama , nim , sesi , alasan , time , date ) VALUES ( '$nama' , '$nim' , '$sesi' , '$alasan' , '$time' , '$date' )");

805709b785f8cd70e30eb30932acdc3a82a80b80
rapidesign <fidrafid@gmail.com>
2017-01-20 16:40:01 +0700

    Add files via upload

---
+++ info.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8" />
+    <meta name="format-detection" content="telephone=no">
+    <meta name="msapplication-tap-highlight" content="no">
+    <meta name="viewport" content="user-scalable=no, initial-scale=1, maximum-scale=1, minimum-scale=1, width=device-width">
+    
+    <!-- No Caching For Development -->
+    <meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate" />
+    <meta http-equiv="Pragma" content="no-cache" />
+    <meta http-equiv="Expires" content="0" />
+      
+    <link rel="icon" type="image/png" href="">
+    <!--Import Google Icon Font-->
+    <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <!--Import materialize.css-->
+    <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
+    <!--Let browser know website is optimized for mobile-->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+     <!--Import jQuery before materialize.js-->
+    <script type="text/javascript" src="js/jquery.js"></script>
+    <script type="text/javascript" src="js/bin/materialize.min.js"></script>
+    
+    <!--CSS-->
+    <link type="text/css" rel="stylesheet" href="css/main.css"/>
+    
+    <title>Registration Form</title>
+    <style>
+        #logo{
+            margin: 7px 0px 0px 7px;
+        }
+    </style>
+  </head>
+    
+    <body>
+        <main class="page-main">
+            <div id="ok">
+                <div class="row">
+                <div class="col s12">
+                    <center><h4>Successfully Sent.</h4></center>
+                    <center><a class="waves-effect waves-light btn blue" href="index.php">Back To Form</a></center>
+                </div>
+                </div>
+            </div>
+        </main>
+    </body>
+</html>
---
+++ validation.php
@@ -0,0 +1,17 @@
+<?php
+	if($_SERVER['REQUEST_METHOD'] = "POST") //Added an if to keep the page secured
+	{   
+        $nama =  $_POST['nama'];
+        $nim =  $_POST['nim'];
+        $sesi =  $_POST['sesi'];
+        $alasan =  $_POST['alasan'];
+		$time = strftime("%X");//time
+        $date = strftime("%B %d, %Y");//date
+
+		$con = mysqli_connect("localhost", "root", "", "test"); //Connect to server and database
+        if (!$con) die("Connection failed: " . mysqli_connect_error()); //check connection
+        
+        mysqli_query($con,"INSERT INTO training_session ( nama , nim , sesi , alasan , time , date ) VALUES ( '$nama' , '$nim' , '$sesi' , '$alasan' , '$time' , '$date' )");
+    }
+    header('Location: info.php');
+?>
